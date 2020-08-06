3be9fdd34bb88872926f1069dba015e22e8aec81
liberjul <liberjul@msu.edu>
2020-08-05 20:34:01 -0400

    sed command fix.

--- constax.sh
+++ constax.sh
@@ -334,7 +334,7 @@ FRM_INPUT=$(python $CONSTAXPATH/check_input_names.py -i "$INPUT" 2>&1)
 $SINTAXPATH -sintax $FRM_INPUT -db "${TFILES}"/sintax.db -tabbedout "$TAX"/otu_taxonomy.sintax -strand both -sintax_cutoff $CONF -threads $NTHREADS
 if [[ $SINTAXPATH == vsearch ]]
 then
-  sed -i 's|([0-1][.][0-9]\{2\}|&00|g' "${TFILES}"/otu_taxonomy.sintax
+  sed -i 's|([0-1][.][0-9]\{2\}|&00|g' "$TAX"/otu_taxonomy.sintax
 fi
 if $BLAST
 then
