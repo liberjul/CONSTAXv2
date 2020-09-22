4b33b00fd6b1a239a31dab3126788cb0b872cd1e
liberjul <liberjul@msu.edu>
2020-09-22 15:59:29 -0400

    Indendation fixes.

--- CombineTaxonomy.py
+++ CombineTaxonomy.py
@@ -449,14 +449,14 @@ def str2bool(v):
 	'''
 	Code borrowed from: https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
 	'''
-    if isinstance(v, bool):
-       return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+	if isinstance(v, bool):
+		return v
+	if v.lower() in ('yes', 'true', 't', 'y', '1'):
+		return True
+	elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+		return False
+	else:
+		raise argparse.ArgumentTypeError('Boolean value expected.')
 ###############################################################################
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ###############################################################################
